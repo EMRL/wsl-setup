59cf649e5690d1468a3af594bb90c6b6039b4d84
Floyd Diebel <fdiebel@emrl.com>
2023-07-01 23:18:00 -0700

    Update wsl-setup.sh

--- wsl-setup.sh
+++ wsl-setup.sh
@@ -180,7 +180,7 @@ function install_wpcli() {
 function install_webmin() {
   if yesno --default yes "Continue? [Y/n] "; then
     curl -fsSL https://download.webmin.com/jcameron-key.asc | sudo gpg --dearmor -o /usr/share/keyrings/webmin.gpg
-    echo 'deb [signed-by=/usr/share/keyrings/webmin.gpg] http://download.webmin.com/download/repository sarge contrib' >> /etc/apt/sources.list
+    sudo echo 'deb [signed-by=/usr/share/keyrings/webmin.gpg] http://download.webmin.com/download/repository sarge contrib' >> /etc/apt/sources.list
     sudo apt update
     sudo apt install webmin -y
   fi
